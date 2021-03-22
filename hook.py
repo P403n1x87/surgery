792748fb7497d8e5d0998a54231fc16742250c55
Gabriele N. Tornetta <phoenix1987@gmail.com>
2021-03-22 12:28:35 +0000

    use the bytecode module for jump resolutions

--- hook.py
+++ hook.py
@@ -1,3 +1,7 @@
-def hook(locs):
-    print(locs)
+from pprint import pprint as print
+from inspect import currentframe
 
+
+def hook(ident):
+    print(f"Breakpoint {ident} has been hit!")
+    print(currentframe().f_back.f_locals)
--- surgery.py
+++ surgery.py
@@ -1,93 +1,60 @@
-from dis import opmap
-from types import CodeType, FunctionType
+from bytecode import Instr, Bytecode
+from types import FunctionType
+from typing import List, Tuple
 
-HOOK_STACKSIZE = 2
 
+def _hook_bytecode(hook, lineno, ident):
+    """The hook receives a breakpoint identifier.
 
-def hook_bytes(names_offset):
-    """The hook will receive the dictionary of locals."""
-    LOAD_GLOBAL = opmap["LOAD_GLOBAL"]
-    CALL_FUNCTION = opmap["CALL_FUNCTION"]
-    POP_TOP = opmap["POP_TOP"]
-    return bytes(
+    This is the equivalent of ``hook(ident)``.
+    """
+    return Bytecode(
         [
-            LOAD_GLOBAL,
-            names_offset,
-            LOAD_GLOBAL,
-            names_offset + 1,
-            CALL_FUNCTION,
-            0,
-            CALL_FUNCTION,
-            1,
-            POP_TOP,
-            0,
+            Instr("LOAD_GLOBAL", hook.__name__, lineno=lineno),
+            Instr("LOAD_CONST", ident, lineno=lineno),
+            Instr("CALL_FUNCTION", 1, lineno=lineno),
+            Instr("POP_TOP", lineno=lineno),
         ]
     )
 
 
-def offset_from_line(line, firstlineno, lnotab):
-    """Find the bytecode offset for the given line."""
-    # TODO: Handle negetive offsets!
-    n = len(lnotab)
-    assert n & 1 == 0
+def _inject_hook(code: Bytecode, h: FunctionType, line: int, ident: int) -> Bytecode:
+    for i, instr in enumerate(code):
+        try:
+            if instr.lineno == line:
+                # gotcha!
+                break
+        except AttributeError:
+            # pseudo-instruction (e.g. label)
+            pass
+    else:
+        raise RuntimeError("Invalid line number. It either is empty or is a comment.")
 
-    l = firstlineno
-    tab = lnotab
-    offset = 0
-    index = 0
-    while tab:
-        index += 1
-        b, d, *tab = tab
-        l += d
-        offset += b
-        if l >= line:
-            return offset, index
-    raise IndexError("Line out of bound")
+    # actual injection
+    code[i:i] = _hook_bytecode(h, line, ident)
 
 
-def inject_hook(f: FunctionType, h: FunctionType, line: int) -> FunctionType:
+def inject_hooks(
+    f: FunctionType, hs: List[Tuple[FunctionType, int, int]]
+) -> FunctionType:
     f_code = f.__code__
+    abstract_code = Bytecode.from_code(f_code)
 
-    # We are adding two global names to the existing ones
-    names_offset = len(f_code.co_names)
-    new_names = f_code.co_names + (h.__name__, "locals")
+    # sanity check
+    assert len(abstract_code)
 
-    # Inject the hook at the given line
-    offset, index = offset_from_line(line, f_code.co_firstlineno, f_code.co_lnotab)
-    hook = hook_bytes(names_offset)
-    new_bytecode = f_code.co_code[:offset] + hook + f_code.co_code[offset:]
+    lo, hi = abstract_code[0].lineno, abstract_code[-1].lineno
+    for line in [_[1] for _ in hs]:
+        if not (lo <= line <= hi):
+            raise RuntimeError(
+                f"Line out of bounds for '{f.__qualname__}'. Valid range is [{lo}-{hi}]"
+            )
 
-    # Make it look like we "inlined" the hook
-    new_lnotab = (
-        f_code.co_lnotab[: index << 1]
-        + bytes(
-            [
-                f_code.co_lnotab[index << 1] + len(hook),
-                f_code.co_lnotab[(index << 1) + 1],
-            ]
-        )
-        + f_code.co_lnotab[(index + 1) << 1 :]
-    )  # FIXME: Index out of range
+    for h, line, ident in hs:
+        _inject_hook(abstract_code, h, line, ident)
 
     return FunctionType(
-        CodeType(
-            f_code.co_argcount,
-            f_code.co_posonlyargcount,
-            f_code.co_kwonlyargcount,
-            f_code.co_nlocals,
-            max(HOOK_STACKSIZE, f_code.co_stacksize),
-            f_code.co_flags,
-            new_bytecode,
-            f_code.co_consts,
-            new_names,
-            f_code.co_varnames,
-            f_code.co_filename,
-            f_code.co_name,
-            f_code.co_firstlineno,
-            new_lnotab,
-            f_code.co_freevars,
-            f_code.co_cellvars,
-        ),
+        abstract_code.to_code(),
         f.__globals__,
         f.__name__,
         f.__defaults__,
@@ -95,10 +62,16 @@ def inject_hook(f: FunctionType, h: FunctionType, line: int) -> FunctionType:
     )
 
 
+def inject_hook(
+    f: FunctionType, h: FunctionType, line: int, ident: int
+) -> FunctionType:
+    return inject_hooks(f, [(h, line, ident)])
+
+
 def inject(hook: FunctionType, line: int) -> FunctionType:
     """decorator."""
 
     def wrapper(f: FunctionType):
-        return inject_hook(f, hook, line)
+        return inject_hook(f, hook, line, 0)
 
     return wrapper
